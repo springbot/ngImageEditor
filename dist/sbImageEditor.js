7c5dc112f1a06279521c9552a3776c35247ad5c7
Dustin Goodman <dustin.s.goodman@gmail.com>
2016-03-28 14:16:36 -0400

    bump version to 0.2.3

--- dist/sbImageEditor.js
+++ dist/sbImageEditor.js
@@ -134,7 +134,7 @@
         _getImageSize(src).then(function (size) {
           imgSize = size;
           overlay.refreshAndRender(img, $scope.selected, imgSize);
-          $scope.onImgChange({ imgSize: imgSize });
+          $scope.onImgChange()(imgSize);
         });
       });
 
